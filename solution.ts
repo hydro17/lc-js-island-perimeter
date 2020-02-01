dcdce54116a19d3911a9de35227fe6e70db00a5a
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2020-02-01 12:50:57 +0100

    iterative solution using an array as a stack

--- solution.ts
+++ solution.ts
@@ -1,32 +1,38 @@
 
 var islandPerimeter = function (grid: number[][]): number {
   let islandPerim = 0;
+  let stack: number[][] = [];
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] === 1) islandPerim = markIslandPieces(i, j, grid, 0);
+      if (grid[i][j] === 1) stack.push([i, j]);
+
+      while (stack.length > 0) {
+        const [i, j] = stack.pop();
+        islandPerim = markIslandPieces(i, j, grid, islandPerim, stack);
+      }
     }
   }
 
   return islandPerim;
 };
 
-const markIslandPieces = (i: number, j: number, grid: number[][], islandPerim: number): number => {
+const markIslandPieces = (i: number, j: number, grid: number[][], islandPerim: number, stack: number[][]): number => {
   if (grid[i][j] === 2) return islandPerim;
   if (grid[i][j] === 0) return ++islandPerim;
 
   grid[i][j] = 2;
 
-  if (i - 1 >= 0) islandPerim = markIslandPieces(i - 1, j, grid, islandPerim);
+  if (i - 1 >= 0) stack.push([i - 1, j]);
   else islandPerim++;
 
-  if (j + 1 < grid[i].length) islandPerim = markIslandPieces(i, j + 1, grid, islandPerim);
+  if (j + 1 < grid[i].length) stack.push([i, j + 1]);
   else islandPerim++;
 
-  if (i + 1 < grid.length) islandPerim = markIslandPieces(i + 1, j, grid, islandPerim);
+  if (i + 1 < grid.length) stack.push([i + 1, j]);
   else islandPerim++;
 
-  if (j - 1 >= 0) islandPerim = markIslandPieces(i, j - 1, grid, islandPerim);
+  if (j - 1 >= 0) stack.push([i, j - 1]);
   else islandPerim++;
 
   return islandPerim;
